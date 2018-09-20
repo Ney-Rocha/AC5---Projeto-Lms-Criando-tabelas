5e6b4d3c875e98137b999f94106ad4e9b09db8cd
rafasato1 <42000212+rafasato1@users.noreply.github.com>
2018-09-20 11:41:20 -0300

    AC7 INSERT COORDENADORES

---
+++ AC7 INSERT COORDENADORES.sql
@@ -0,0 +1,9 @@
+--Adicionando Coordenadores na tabela COORDENADOR--
+
+INSERT INTO COORDENADOR (IDUSUARIO, NOME, EMAIL, CELULAR)VALUES
+(65, 'FRANCESCA ROMANELLI', 'f_romanelli@impacta.com.br','11935612587'),
+(56, 'ANA CRISTINA DOS SANTOS','ac_santos@impacta.com.br','11965325167'),
+(89, 'BRUNO LUIS SOARES DE LIMA','b_lima@impacta.com.br','11965771258'),
+(80, 'OSVALDO KOTARO TAKAI','ok_takai@impacta.com.br','11963258147'),
+(90, 'ALAN HENRIQUE PARDO DE CARVALHO','al_carvalho@impacta.com.br','11965846576'),
+(91, 'AYAO OKAMOTO', 'ay_okamoto@impacta.com.br','11968541235')
