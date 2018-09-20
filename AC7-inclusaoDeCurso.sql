731588ee3691ce07673b7433f6ca7208016bdb9f
RamonFontana <fontanaramon@gmail.com>
2018-09-20 10:57:50 -0300

    Adicionado inserção de alunos e curso

---
+++ AC7-inclusaoDeCurso.sql
@@ -0,0 +1,34 @@
+--Cadastro de Cursos
+INSERT INTO CURSO(NOME) 
+    VALUES ('Administração');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Análise e Desenvolvimento de Sistemas');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Análise e Desenvolvimento de Sistemas EAD');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Banco de Dados');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Engenharia da Computação');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Gestão de Tecnologia da Informação');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Jogos Digitais');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Processo Gerenciais');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Produção de Multimídia');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Redes de Computadores');
+
+INSERT INTO CURSO(NOME) 
+    VALUES ('Sistema de Informação');
+
